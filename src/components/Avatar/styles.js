36108e39ea3b2ca4b0ea4e91181eed372138810f
LucasSlv <lucassilvax005x@gmail.com>
2022-10-13 09:51:09 -0300

    feat: criando componente Avatar

---
+++ src/components/Avatar/styles.js
@@ -0,0 +1,36 @@
+import styled, { css } from 'styled-components';
+
+const modifierContainer = {
+  small: () => css`
+    width: 53.31px;
+    height: 53.31px;
+
+    img {
+      width: 20px;
+      height: 30px;
+    }
+  `,
+
+  big: () => css`
+    width: 76px;
+    height: 76px;
+
+    img {
+      width: 32px;
+      height: 32px;
+    }
+  `,
+};
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ backgroundColor }) => backgroundColor};
+  border-radius: 100%;
+
+  ${({ size }) => css`
+    ${console.log(size)}
+    ${!!size && modifierContainer[size]()}
+  `}
+`;
