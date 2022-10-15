03366aa9fd2b7f7a253d9c9620893f2d4bd199d1
LucasSlv <lucassilvax005x@gmail.com>
2022-10-15 13:43:39 -0300

    chore: ajustando componente Logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,6 +4,7 @@ export const Logo = () => {
   return (
     <Container>
       <img src="/assets/images/logo.png" alt="logo" />
+      <p> O lugar ideal para quem busca produtos orgânicos, direto com o produtor. </p>
     </Container>
   );
 };
--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -6,10 +6,17 @@ export const Container = styled.div`
   align-items: center;
   background: #ebf1e7;
   padding: 32px 206px 32px 33px;
+  gap: 206px;
 
   img {
     width: 97.64px;
     height: 108.39px;
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.typography.h2.fontSize};
+    color: ${({ theme }) => theme.palettes.secondaryStraw.main};
+    word-wrap: break-word;
   }
 
   @media (max-width: 768px) {
