0d80d734a5dbff2ba7aa31490a88679cb26bcb0c
LucasSlv <lucassilvax005x@gmail.com>
2022-10-23 13:33:34 -0300

    feat: ProfileSeller component

--- src/components/ProfileSeller/index.js
+++ src/components/ProfileSeller/index.js
@@ -1,26 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSeller } from '../../services/sellerService';
 import { Avatar } from '../Avatar';
 import { useTheme } from 'styled-components';
 import { Container, Wrapper, NameSeller, InfoSeller, Km, Box, Crate } from './styles';
 import { Link } from 'react-router-dom';
 
-const mockSeller = {
-  contact: ['Horta da Dona Clara', 'Aqui você encontra os melhores legumes'],
-  address: ['São Paulo', 'Centro', 'Rua dos bobo', '1'],
-  rating: ['3,7'],
-};
+export const CardRickAndMorty = ({ to }) => {
+  const [seller, setSeller] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const { id } = useParams();
 
-export const ProfileSeller = ({ to }) => {
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await getSeller(id);
+        setSeller(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [id]);
+
+  if (isError) {
+    return <div>Ooops! Algo de errado aconteceu!</div>;
+  }
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
   const theme = useTheme();
   return (
     <Container>
       <Wrapper>
         <Avatar size="big" backgroundColor={theme.palettes.primaryGreen.v4} />
         <Box>
-          <NameSeller>{mockSeller.contact[0]} </NameSeller>
+          <NameSeller>{seller.contact.name} </NameSeller>
           <InfoSeller>
-            {mockSeller.address[2]}, n*{mockSeller.address[3]} - {mockSeller.address[1]}, {mockSeller.address[0]}
+            {seller.contact.address.street}, n*{seller.contact.address.number} - {seller.contact.address.neighborhood},
+            {seller.contact.address.city}
           </InfoSeller>
-          <Km>{mockSeller.rating[0]}km</Km>
+          <Km>{seller.rating[0]}km</Km>
         </Box>
       </Wrapper>
       <Crate>
