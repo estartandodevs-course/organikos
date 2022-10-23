0a37df00427e93d297778457dff9feec57f69e00
LucasSlv <lucassilvax005x@gmail.com>
2022-10-23 20:09:57 -0300

    chore: ajustando componente Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,11 +1,11 @@
 import { Container } from './styles';
 import { Link } from 'react-router-dom';
 
-export const Title = ({ text, icon, to }) => {
+export const Title = ({ text, icon, to = '#' }) => {
   return (
     <Container icon={icon}>
       <Link to={to}>
-        <img src="assets/icons/back-icon.svg" alt="back icon" />
+        <img src="../assets/icons/back-icon.svg" alt="back icon" />
       </Link>
       <p>{text}</p>
     </Container>
