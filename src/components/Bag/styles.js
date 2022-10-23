5c0cfac53eafb08833f8774dd12bb223e237be25
LucasSlv <lucassilvax005x@gmail.com>
2022-10-23 20:08:29 -0300

    feat: adicionando o component Bag

---
+++ src/components/Bag/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+  margin-bottom: 16px;
+
+  p {
+    font-weight: 500;
+    color: ${({ theme }) => theme.palettes.secondaryPurple.main};
+  }
+`;
