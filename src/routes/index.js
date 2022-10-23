7d3667ce24225e024f145cfabe7be087cc00bdf6
LucasSlv <lucassilvax005x@gmail.com>
2022-10-23 20:10:26 -0300

    feat: adicionando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages';
+import { Seller } from '../pages/Seller';
 
 export const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/seller/:id" element={<Seller />} />
+      <Route path="/bag" element={<p>carrinho</p>} />
+      <Route path="/checkout/:id" element={<p>Finalizando pedido</p>} />
+      <Route path="/details/:id" element={<p>Detalhes da compra</p>} />
+      <Route path="/feedback" element={<p>Avaliação</p>} />
+      <Route path="/historic" element={<p>Historico</p>} />
     </Routes>
   );
 };
