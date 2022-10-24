ef7c303325cdbdee4125bd40358be87f3f564c50
LucasSlv <lucassilvax005x@gmail.com>
2022-10-23 21:15:59 -0300

    chore: ajustando componente Logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,20 +1,23 @@
 import { Container, Wrapper, Image, Title, Box } from './styles';
 import { useTheme } from 'styled-components';
 import { Avatar } from '../Avatar';
+import { Link } from 'react-router-dom';
 
-export const Logo = () => {
+export const Logo = ({ icon, to }) => {
   const theme = useTheme();
 
   return (
     <Container>
-      <Image src="/assets/images/logo.png" alt="logo" />
+      <Image src="../assets/images/logo.png" alt="logo" />
       <Title> O lugar ideal para quem busca produtos orgânicos, direto com o produtor. </Title>
       <Box>
         <Wrapper>
           <Avatar size="small" backgroundColor={theme.palettes.secondaryPurple.v4} />
           <p>Usuário</p>
         </Wrapper>
-        <img src="assets/icons/cart.svg" alt="icon car" />
+        <Link to={to}>
+          <img src={`../assets/icons/icons-profile/${icon}.svg`} alt="icon car" />
+        </Link>
       </Box>
     </Container>
   );
