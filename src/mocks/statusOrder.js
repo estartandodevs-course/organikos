245ace4a4d173b08230ab5e42f6ff7b5e6a431ef
pablokaua <pablokauaah@gmail.com>
2022-10-24 20:41:48 -0300

    refactor: refatorando mocks dos componentes

---
+++ src/mocks/statusOrder.js
@@ -0,0 +1,5 @@
+export const StatusOrder = {
+  processing: ['../assets/icons/icons-requests/hourglass.svg', 'Processando'],
+  done: ['../assets/icons/icons-requests/confirmation-requests.svg', 'Aprovado'],
+  cancelled: ['../assets/icons/icons-requests/deny.svg', 'Recusado'],
+};
