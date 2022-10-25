f775fdae14554e815218ebafcbe3c62eeab70acd
LucasSlv <lucassilvax005x@gmail.com>
2022-10-24 22:07:40 -0300

    chore: ajustando componente Checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -5,40 +5,11 @@ import { Footer } from '../../components/Footer';
 import { Logo } from '../../components/Logo';
 import { Title } from '../../components/Title';
 import { Box, Container, Wrapper } from './styles';
+import { infoOrder } from '../../mocks/infoOrder';
 
 export const Checkout = () => {
   const { id } = useParams();
   const theme = useTheme();
-  const mock = [
-    [
-      '+55 079 999646105',
-      'Retirada',
-      'Pix',
-      ['cenoura - 200g', 'berinjela - 200g', 'tomate - 200g', 'maça - 200g'],
-      '200',
-    ],
-    [
-      '+55 079 999646105',
-      'Retirada',
-      'Pix',
-      ['cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g'],
-      '200',
-    ],
-    [
-      '+55 079 999646105',
-      'Retirada',
-      'Pix',
-      ['cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g'],
-      '200',
-    ],
-    [
-      '+55 079 999646105',
-      'Retirada',
-      'Pix',
-      ['cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g', 'cenoura - 200g'],
-      '200',
-    ],
-  ];
 
   return (
     <Container>
@@ -47,19 +18,19 @@ export const Checkout = () => {
       <Wrapper>
         <p>
           Fale com o produtor:
-          <span>{mock[0][0]}</span>
+          <span>{infoOrder[0][0]}</span>
         </p>
         <p>
           Forma de entrega:
-          <span>{mock[0][1]}</span>
+          <span>{infoOrder[0][1]}</span>
         </p>
         <p>
           Forma de pagamento:
-          <span>{mock[0][2]}</span>
+          <span>{infoOrder[0][2]}</span>
         </p>
         <ul>
           <p>Detalhes da compra:</p>
-          {mock[0][3].map(item => (
+          {infoOrder[0][3].map(item => (
             <>
               <br />
               <li key={item}>{item}</li>
@@ -68,7 +39,7 @@ export const Checkout = () => {
         </ul>
         <h3>
           Total da compra:
-          <span>R${mock[0][4]}</span>
+          <span>R${infoOrder[0][4]}</span>
         </h3>
       </Wrapper>
       <Box>
