32a43b2e85ce3a8bd25046b0d55f2296841fd265
pablokaua <pablokauaah@gmail.com>
2022-10-24 21:13:54 -0300

    feat: Adicionando historic page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages';
 import { Checkout } from '../pages/Checkout';
+import { Historic } from '../pages/Historic';
 import { Seller } from '../pages/Seller';
 
 export const Routers = () => {
@@ -12,7 +13,7 @@ export const Routers = () => {
       <Route path="/checkout/:id" element={<Checkout />} />
       <Route path="/details/:id" element={<p>Detalhes da compra</p>} />
       <Route path="/feedback" element={<p>Avaliação</p>} />
-      <Route path="/historic" element={<p>Historico</p>} />
+      <Route path="/historic" element={<Historic />} />
     </Routes>
   );
 };
