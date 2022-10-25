fec1180fea0bd313c3c8604079d640f5a906e2f5
pablokaua <pablokauaah@gmail.com>
2022-10-25 12:15:59 -0300

    chore: ajustando componente ProductCard

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,12 +1,12 @@
 import { ImageContainer, InfoProduct, Wrapper } from './styles';
 
-export const ProductCard = ({ product }) => {
+export const ProductCard = ({ product, size }) => {
   return (
     <Wrapper>
-      <ImageContainer>
+      <ImageContainer size={size}>
         <img src="/assets/icons/icons-amount/fewer.svg" alt="icon menos" />
         <img src={product[0]} alt={`Imagem ${product[1]}`} />
-        <img src="/assets/icons/icons-amount/plus.svg" alt="icon menos" />
+        <img src="/assets/icons/icons-amount/plus.svg" alt="icon mais" />
       </ImageContainer>
       <InfoProduct>
         <p>{product[1]}</p>
---
+++ src/mocks/historicProducts.js
@@ -0,0 +1,6 @@
+export const historicProducts = [
+  ['/assets/images/images-products/banana.png', 'Banana', '100g', 'R$00,00'],
+  ['/assets/images/images-products/carrot.png', 'Carrot', '100g', 'R$00,00'],
+  ['/assets/images/images-products/chili.png', 'Chili', '100g', 'R$00,00'],
+  ['/assets/images/images-products/eggplant.png', 'Eggplant', '100g', 'R$00,00'],
+];
