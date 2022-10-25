d2ff7882482bc8fdca67732d7f186d3ef488e185
Ramon <ramon1234rxm@gmail.com>
2022-10-24 22:23:29 -0300

    fix: adicionando height no footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const FooterWrapper = styled.footer`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
