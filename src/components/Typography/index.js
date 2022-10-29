8273b1c17fa47d4a2a9f36a41eb43662b2e34c02
pablokaua <pablokauaah@gmail.com>
2022-10-29 15:05:46 -0300

    chore: ajustando componente Typography

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -1,7 +1,11 @@
 import { Typographies } from './styles';
 
-export const Typography = ({ children, varient, color }) => {
-  const Element = Typographies[varient];
+export const Typography = ({ children, variant, color }) => {
+  const Element = Typographies[variant];
 
   return <Element color={color}>{children}</Element>;
+};
+
+Typography.defaultProps = {
+  variant: 'h1Regular',
 };
