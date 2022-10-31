aa5aa4ae4f1ce309d10ab146bfa1734d3a75efbb
pablokaua <pablokauaah@gmail.com>
2022-10-31 16:57:15 -0300

    chore: adicionando responsividade na page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,20 @@
 import { Logo } from '../../components/Logo/index';
 import { InputRange } from '../../components/InputRange/index';
 import { InputSearch } from '../../components/InputSearch/index';
-import { Container, Wrapper } from './styles';
+import { Container, FilterWrapper, Wrapper } from './styles';
 import { SellerList } from '../../components/SellerList';
 import { Footer } from '../../components/Footer';
+import { categories } from '../../mocks/categories';
+import { Tags } from '../../components/Tags';
 
 export const Home = () => {
   return (
     <Container>
       <Logo icon="bag" to="/bag" />
       <InputRange initialRange={2} />
+      <FilterWrapper>
+        <Tags categorys={categories} />
+      </FilterWrapper>
       <InputSearch />
       <Wrapper>
         <SellerList />
