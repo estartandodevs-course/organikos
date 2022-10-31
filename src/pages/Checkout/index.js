1a97d9071d38fbbf1fd4f3eb538003513d52ea84
LucasSlv <lucassilvax005x@gmail.com>
2022-10-31 16:37:12 -0300

    chore: ajustando responsividade da page Checkout

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -4,8 +4,9 @@ import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 import { Logo } from '../../components/Logo';
 import { Title } from '../../components/Title';
-import { Box, Container, Wrapper } from './styles';
+import { Box, Container, Crate, Receptacle, Wrapper } from './styles';
 import { infoOrder } from '../../mocks/infoOrder';
+import { InputSearch } from '../../components/InputSearch';
 
 export const Checkout = () => {
   const { id } = useParams();
@@ -14,44 +15,49 @@ export const Checkout = () => {
   return (
     <Container>
       <Logo icon="home" to="/" />
+      <Receptacle>
+        <InputSearch />
+      </Receptacle>
       <Title icon="block" text="Informações sobre a compra" to="/seller/:id" />
-      <Wrapper>
-        <p>
-          Fale com o produtor:
-          <span>{infoOrder[0][0]}</span>
-        </p>
-        <p>
-          Forma de entrega:
-          <span>{infoOrder[0][1]}</span>
-        </p>
-        <p>
-          Forma de pagamento:
-          <span>{infoOrder[0][2]}</span>
-        </p>
-        <ul>
-          <p>Detalhes da compra:</p>
-          {infoOrder[0][3].map(item => (
-            <>
-              <br />
-              <li key={item}>{item}</li>
-            </>
-          ))}
-        </ul>
-        <h3>
-          Total da compra:
-          <span>R${infoOrder[0][4]}</span>
-        </h3>
-      </Wrapper>
-      <Box>
-        <Link to={`/details/${id}`}>
-          <Button backgroundColor={theme.palettes.secondaryPurple.main}>Cofirmar Pedido</Button>
-        </Link>
-        <Link to={`/seller/${id}`}>
-          <Button backgroundColor={theme.palettes.neutral.v2} color={theme.palettes.black}>
-            Cancelar
-          </Button>
-        </Link>
-      </Box>
+      <Crate>
+        <Wrapper>
+          <p>
+            Fale com o produtor:
+            <span>{infoOrder[0][0]}</span>
+          </p>
+          <p>
+            Forma de entrega:
+            <span>{infoOrder[0][1]}</span>
+          </p>
+          <p>
+            Forma de pagamento:
+            <span>{infoOrder[0][2]}</span>
+          </p>
+          <ul>
+            <p>Detalhes da compra:</p>
+            {infoOrder[0][3].map(item => (
+              <>
+                <br />
+                <li key={item}>{item}</li>
+              </>
+            ))}
+          </ul>
+          <h3>
+            Total da compra:
+            <span>R${infoOrder[0][4]}</span>
+          </h3>
+        </Wrapper>
+        <Box>
+          <Link to={`/details/${id}`}>
+            <Button backgroundColor={theme.palettes.secondaryPurple.main}>Cofirmar Pedido</Button>
+          </Link>
+          <Link to={`/seller/${id}`}>
+            <Button backgroundColor={theme.palettes.neutral.v2} color={theme.palettes.black}>
+              Cancelar
+            </Button>
+          </Link>
+        </Box>
+      </Crate>
       <Footer>Organikos</Footer>
     </Container>
   );
