25247abbd6f371f95d42d13967d6f74d4c6c819c
LucasSlv <lucassilvax005x@gmail.com>
2022-11-07 18:45:14 -0300

    chore: ajustando page BagPage

--- src/pages/BagPage/index.js
+++ src/pages/BagPage/index.js
@@ -2,21 +2,15 @@ import { BackButton } from '../../components/BackButton';
 import { Footer } from '../../components/Footer';
 import { ProfileSeller } from '../../components/ProfileSeller';
 import { Title } from '../../components/Title';
-import { Box, Container, Wrapper } from './styles';
-import { bagMock } from '../../mocks/bag';
-import { ProductCard } from '../../components/ProductCard';
-import { v4 as uuidv4 } from 'uuid';
+import { Box, Container } from './styles';
+import { ProductsCardWrapper } from '../../components/ProductsCardWrapper';
 
 export const BagPage = () => {
   return (
     <Container>
       <ProfileSeller />
       <Title text="Carrinho de compras - R$102,50" icon="none" />
-      <Wrapper>
-        {bagMock.map(product => (
-          <ProductCard key={product[1] + uuidv4()} product={product} size="20px" />
-        ))}
-      </Wrapper>
+      <ProductsCardWrapper />
       <Box>
         <BackButton>Voltar para a compra</BackButton>
       </Box>
--- src/pages/BagPage/styles.js
+++ src/pages/BagPage/styles.js
@@ -8,15 +8,6 @@ export const Container = styled.div`
   }
 `;
 
-export const Wrapper = styled.section`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 25px;
-  flex-wrap: wrap;
-  margin: 10px 0px 85px;
-`;
-
 export const Box = styled.div`
   width: 240px;
   margin: 0 auto;
