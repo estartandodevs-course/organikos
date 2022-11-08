d7586b6cdff9fae1f2ff7e85acd8adec4a3b59ca
pablokaua <pablokauaah@gmail.com>
2022-11-08 16:00:13 -0300

    chore: ajustando filtros SellerList

--- src/components/SellerList/index.js
+++ src/components/SellerList/index.js
@@ -8,30 +8,26 @@ import { FilterContext } from '../../contexts/FilterContext';
 
 export const SellerList = () => {
   const { sellerList, isLoading } = useContext(SellersContext);
-  const { searchTerm, tagFilter } = useContext(FilterContext);
+  const { searchTerm } = useContext(FilterContext);
+
+  const sellerFiltered = sellerList.filter(seller => {
+    if (searchTerm === '') {
+      return seller;
+    } else if (seller?.contact?.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return seller;
+    }
+  });
 
   return (
     <Ul>
       {isLoading && <CardSellerSkeleton number={12} />}
-      {sellerList
-        .filter(seller => {
-          if (searchTerm === '' && tagFilter.length === 0) {
-            return seller;
-          } else if (
-            seller?.contact?.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
-            seller?.category.some(el => tagFilter.includes(el))
-          ) {
-            return seller;
-          }
-        })
-
-        ?.map(seller => {
-          return (
-            <li key={seller.sellerId + uuidv4()}>
-              <CardSeller seller={seller} />
-            </li>
-          );
-        })}
+      {sellerFiltered?.map(seller => {
+        return (
+          <li key={seller.sellerId + uuidv4()}>
+            <CardSeller seller={seller} />
+          </li>
+        );
+      })}
     </Ul>
   );
 };
