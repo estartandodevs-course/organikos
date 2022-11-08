20f81d1ca61ac607788bb66f1234848a0d87a49b
pablokaua <pablokauaah@gmail.com>
2022-11-08 13:23:18 -0300

    chore: adicionando interatividade Tags

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -1,11 +1,29 @@
 import { Container, Tag, Wrapper } from './styles';
 import { tags } from '../../mocks/tags';
+import { useContext } from 'react';
+import { FilterContext } from '../../contexts/FilterContext';
 
-export const Tags = ({ categorys }) => {
+export const Tags = ({ categorys, touchable = false }) => {
+  const { handleIsTouchable, tagFilter } = useContext(FilterContext);
+
   return (
     <Container>
       {categorys?.map(currentCategory => (
-        <Wrapper key={currentCategory} backgroundColor={tags[currentCategory]?.[0]}>
+        <Wrapper
+          key={currentCategory}
+          backgroundColor={() => {
+            if (!touchable) {
+              return tags[currentCategory]?.[0];
+            } else if (tagFilter.includes(currentCategory)) {
+              return '#923FA5';
+            } else {
+              return tags[currentCategory]?.[0];
+            }
+          }}
+          onClick={() => {
+            if (touchable) handleIsTouchable(currentCategory);
+          }}
+        >
           <img src={tags[currentCategory]?.[1]} />
           <Tag>{currentCategory}</Tag>
         </Wrapper>
