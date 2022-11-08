c97e2ad949260ae0ec75a660f63895b109f05c25
LucasSlv <lucassilvax005x@gmail.com>
2022-11-08 11:59:42 -0300

    chore: ajustando component ProductsCardWrapper

--- src/components/ProductsCardWrapper/styles.js
+++ src/components/ProductsCardWrapper/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
-  align-items: center;
   justify-content: center;
   width: 416px;
   height: 436px;
+  overflow-y: scroll;
   background: ${({ backGroundColor }) => backGroundColor};
+  border-radius: 10px;
 `;
 
 export const Wrapper = styled.section`
@@ -14,4 +15,5 @@ export const Wrapper = styled.section`
   justify-content: space-around;
   flex-wrap: wrap;
   gap: 16px;
+  padding: 16px 0;
 `;
