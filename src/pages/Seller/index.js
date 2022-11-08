1fa665eec8fae043b92532edc9c282a1f84dbd72
LucasSlv <lucassilvax005x@gmail.com>
2022-11-08 12:01:09 -0300

    chore: ajustando responsividade da Page Seller

--- src/pages/Seller/index.js
+++ src/pages/Seller/index.js
@@ -1,19 +1,26 @@
 import { InputSearch } from '../../components/InputSearch';
-import { Title } from '../../components/Title/index';
+// import { Title } from '../../components/Title/index';
 import { ProfileSeller } from '../../components/ProfileSeller';
-import { Bag } from '../../components/Bag';
+// import { Bag } from '../../components/Bag';
 import { Checklist } from '../../components/Checklist';
 import { Link, useParams } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { getSeller } from '../../services/sellerService';
 import { InputForm } from '../../components/InputForm';
-import { Box, Container, Wrapper } from './styles';
+import { Bin, Box, Bunker, Case, Container, Crate, FilterWrapper, Kit, Left, Right, Safe } from './styles';
 import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 import { useTheme } from 'styled-components';
 import { ProductCardList } from '../../components/ProductCardList';
 import { Modal } from '../../components/Modal';
 import { CartContext } from '../../contexts/CartContext';
+import { Tags } from '../../components/Tags';
+import { categories } from '../../mocks/categories';
+import { ProductsCardWrapper } from '../../components/ProductsCardWrapper';
+import { Logo } from '../../components/Logo';
+import { Title } from '../../components/Title';
+import { Bag } from '../../components/Bag';
+import { InfoSeller } from '../../components/InfoSeller';
 
 export const Seller = () => {
   const [seller, setSeller] = useState({});
@@ -44,35 +51,59 @@ export const Seller = () => {
   if (isLoading) {
     return <></>;
   }
-
   return (
     <Container>
-      <ProfileSeller to="/" />
+      <Kit>
+        <Logo icon="bag" />
+      </Kit>
+      <Case>
+        <ProfileSeller to="/" />
+      </Case>
       <InputSearch />
-      <ProductCardList />
-      <Title text="Total da compra - R$102,50" />
-      <Bag />
-      <Title text="Formas de Entrega" />
-      <Checklist optionsList={seller?.distribution} name="entrega" />
-      <Title text="Formas de Pagamento" />
-      <Checklist optionsList={seller?.payment} name="pagamento" />
-      <Wrapper>
-        <InputForm size="medium" type="text" text="Insira aqui seu cupom de desconto" />
-      </Wrapper>
-      <Box>
-        <Link to={`/checkout/${id}`}>
-          <Button backgroundColor={theme.palettes.secondaryPurple.main}> Comprar </Button>
-        </Link>
-        <Link to="/">
-          <Button
-            backgroundColor={theme.palettes.neutral.v2}
-            color={theme.palettes.black}
-            onClick={() => removeAllProductsBySeller(seller)}
-          >
-            Cancelar
-          </Button>
-        </Link>
-      </Box>
+      <Crate>
+        <Left>
+          <InfoSeller to="/" />
+          <ProductsCardWrapper backGroundColor={theme.palettes.primaryGreen.v5} />
+        </Left>
+        <Right>
+          <FilterWrapper>
+            <Tags categorys={categories} />
+          </FilterWrapper>
+          <ProductCardList />
+          <Bin>
+            <Title text="Total da compra - R$102,50" />
+            <Bag />
+            <Title text="Formas de Entrega" />
+          </Bin>
+          <h5>Formas de Entrega</h5>
+          <Checklist optionsList={seller?.distribution} name="entrega" />
+          <Bin>
+            <Title text="Formas de Pagamento" />
+          </Bin>
+          <h5>Formas de Pagamento</h5>
+          <Checklist optionsList={seller?.payment} name="pagamento" />
+          <Safe>
+            <InputForm size="medium" type="text" text="Insira aqui seu cupom de desconto" />
+            <Bunker>
+              <h4>Total da compra - R$102,50</h4>
+              <Box>
+                <Link to={`/checkout/${id}`}>
+                  <Button backgroundColor={theme.palettes.secondaryPurple.main}> Comprar </Button>
+                </Link>
+                <Link to="/">
+                  <Button
+                    onClick={() => removeAllProductsBySeller(seller)}
+                    backgroundColor={theme.palettes.neutral.v2}
+                    color={theme.palettes.black}
+                  >
+                    Cancelar
+                  </Button>
+                </Link>
+              </Box>
+            </Bunker>
+          </Safe>
+        </Right>
+      </Crate>
       <Footer> Organikos </Footer>
       <Modal name={seller.contact.name}>{seller.contact.desc}</Modal>
     </Container>
--- src/pages/Seller/styles.js
+++ src/pages/Seller/styles.js
@@ -1,19 +1,128 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  overflow-x: hidden;
+  width: 100%;
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 40px;
+export const Case = styled.div`
+  display: none;
+
+  @media (max-width: 890px) {
+    display: block;
+  }
+`;
+
+export const Bin = styled.div`
+  display: none;
+
+  @media (max-width: 890px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Kit = styled.div`
+  @media (max-width: 890px) {
+    display: none;
+  }
 `;
 
 export const Box = styled.div`
   display: flex;
-  flex-direction: column;
   gap: 16px;
-  width: 179px;
-  margin: 32px auto 88px auto;
+
+  @media (max-width: 890px) {
+    flex-direction: column;
+  }
+`;
+
+export const FilterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 32px;
+
+  & > ul {
+    display: flex;
+    gap: 40px;
+  }
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const Right = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  h5 {
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.typography.h3.fontSize};
+
+    @media (max-width: 890px) {
+      display: none;
+    }
+  }
+`;
+
+export const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  *::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.palettes.secondaryPurple.v6};
+  }
+
+  *::-webkit-scrollbar {
+    width: 16px;
+  }
+
+  @media (max-width: 1300px) {
+    display: none;
+  }
+`;
+
+export const Crate = styled.div`
+  display: flex;
+  margin: 50px;
+  gap: 50px;
+  margin-top: 50px;
+
+  @media (max-width: 890px) {
+    margin: 16px 0 88px 0;
+  }
+`;
+
+export const Safe = styled.div`
+  display: flex;
+  margin-top: 40px;
+  align-items: flex-end;
+  gap: 146px;
+
+  @media (max-width: 890px) {
+    width: 262px;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px auto 0 auto;
+    gap: 32px;
+  }
+`;
+
+export const Bunker = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+
+  h4 {
+    font-weight: 500;
+    font-size: 32px;
+
+    @media (max-width: 890px) {
+      display: none;
+    }
+  }
 `;
