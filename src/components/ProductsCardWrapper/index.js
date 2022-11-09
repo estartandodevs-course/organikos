7e6c8288b4f34562bd83bf89eff8de5d4be9575a
pablokaua <pablokauaah@gmail.com>
2022-11-09 16:30:08 -0300

    chore: ajustando consumo context

--- src/components/ProductsCardWrapper/index.js
+++ src/components/ProductsCardWrapper/index.js
@@ -4,14 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/CartContext';
 
-export const ProductsCardWrapper = ({ backGroundColor, productsCart, id }) => {
-  const productsCartFiltered = productsCart.filter(product => product.seller_id === id);
-  const { addProductToCart, removeProductToCart } = useContext(CartContext);
-
+export const ProductsCardWrapper = ({ backGroundColor, id }) => {
+  const { addProductToCart, removeProductToCart, getProductsBySeller } = useContext(CartContext);
+  const products = getProductsBySeller(id);
   return (
     <Container backGroundColor={backGroundColor}>
       <Wrapper>
-        {productsCartFiltered.map(product => (
+        {products.map(product => (
           <ProductCard
             key={product[1] + uuidv4()}
             product={product}
