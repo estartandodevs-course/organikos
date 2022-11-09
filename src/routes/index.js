1aa58b1c6b48655af759c5a6ec4d0c13d45bc8b6
Ramon <ramon1234rxm@gmail.com>
2022-11-09 09:43:40 -0300

    fix: routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
-import { routers } from './routers';
+import { BagPage, Checkout, Details, Feedback, Historic, Home, Seller } from '../pages';
 
 export const Routers = () => {
   return (
     <Routes>
-      {routers.map(route => (
-        <Route key={route.path} {...route} />
-      ))}
+      <Route path="/" element={<Home />} />
+      <Route path="/seller/:id" element={<Seller />} />
+      <Route path="/bag" element={<BagPage />} />
+      <Route path="/checkout/:id" element={<Checkout />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/historic" element={<Historic />} />
     </Routes>
   );
 };
