8425c14733005ae36722c0efeb366153d7399cc2
pablokaua <pablokauaah@gmail.com>
2022-11-09 09:47:07 -0300

    chore: adicionando props ProductCardWrapper

--- src/components/ProductsCardWrapper/index.js
+++ src/components/ProductsCardWrapper/index.js
@@ -1,14 +1,24 @@
-import { bagMock } from '../../mocks/bag';
 import { ProductCard } from '../ProductCard';
 import { Container, Wrapper } from './styles';
 import { v4 as uuidv4 } from 'uuid';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 
-export const ProductsCardWrapper = ({ backGroundColor }) => {
+export const ProductsCardWrapper = ({ backGroundColor, productsCart, id }) => {
+  const productsCartFiltered = productsCart.filter(product => product.seller_id === id);
+  const { addProductToCart, removeProductToCart } = useContext(CartContext);
+
   return (
     <Container backGroundColor={backGroundColor}>
       <Wrapper>
-        {bagMock.map(product => (
-          <ProductCard key={product[1] + uuidv4()} product={product} size="20px" />
+        {productsCartFiltered.map(product => (
+          <ProductCard
+            key={product[1] + uuidv4()}
+            product={product}
+            size="20px"
+            onPlus={() => addProductToCart(product)}
+            onMinus={() => removeProductToCart(product)}
+          />
         ))}
       </Wrapper>
     </Container>
