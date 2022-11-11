3d588ee0aced6a64569cb9c9f2b1fdb9f2f2499e
pablokaua <pablokauaah@gmail.com>
2022-11-11 08:58:53 -0300

    feat: adicionando Login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { BagPage, Checkout, Details, Feedback, Historic, Home, Seller } from '../pages';
+import { Login } from '../pages/Login';
 
 export const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/seller/:id" element={<Seller />} />
       <Route path="/seller/:id/checkout" element={<Checkout />} />
       <Route path="/bag" element={<BagPage />} />
